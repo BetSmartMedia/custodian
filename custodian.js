f7861a7827ef4f2808a38b59a85184a00ffc066a
Judd Vinet <judd@betsmartmedia.com>
2014-02-07 02:42:56 -0800

    normalized semicolon usage

--- custodian.js
+++ custodian.js
@@ -13,6 +13,7 @@
  * Custodian also provides basic watchdog functionality. If a process is not
  * running, it will be restarted.
  */
+
 var util       = require("util");
 var cproc      = require("child_process");
 var fs         = require("fs");
@@ -27,14 +28,14 @@ var HOSTNAME = require('os').hostname();
 
 if (require.main === module) {
 	var log = function (str) {
-		var now = dateFormat(new Date, "yyyy-mm-dd HH:MM:ss");
+		var now = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
 		var msg = "[" + now + "] " + str;
 		console.log(msg);
 	};
-	main()
+	main();
 } else {
 	// for unit tests
-	var log = function () {}
+	var log = function() {};
 	exports.run = run;
 }
 
@@ -42,7 +43,7 @@ if (require.main === module) {
  * This function encapsulate reading/reloading config files and application
  * state then starts up the run loop.
  */
-function main () {
+function main() {
 	/**
 	 * Read and parse config
 	 */
@@ -56,7 +57,7 @@ function main () {
 	var CONFIG = {};
 	var STATE  = {schedule:{}, watch:{}};
 
-	process.env.IN_CUSTODIAN = 1
+	process.env.IN_CUSTODIAN = 1;
 
 	load_config(true);
 
@@ -70,21 +71,21 @@ function main () {
 	/**
 	 * Load configuration
 	 */
-	function load_config (exitOnFailure) {
+	function load_config(exitOnFailure) {
 		try {
 			var newConfig = JSON.parse(fs.readFileSync(CFG_FILE, "utf8"));
 			// parsed ok, clobber old config
-			for (var k in CONFIG) delete CONFIG[k];
-			for (var k in newConfig) CONFIG[k] = newConfig[k];
-			if (!CONFIG.schedule) CONFIG.schedule = {}
-			if (!CONFIG.watch) CONFIG.watch = {}
-			if (!CONFIG.env) CONFIG.env = {}
-			CONFIG.env.__proto__ = process.env
-			// setTimeout avoids printing before daemonization we hope
-			setTimeout(log.bind(null, "config ok"), 1000)
+			for(var k in CONFIG) delete CONFIG[k];
+			for(var k in newConfig) CONFIG[k] = newConfig[k];
+			if(!CONFIG.schedule) CONFIG.schedule = {};
+			if(!CONFIG.watch) CONFIG.watch = {};
+			if(!CONFIG.env) CONFIG.env = {};
+			CONFIG.env.__proto__ = process.env;
+			// setTimeout avoids printing before daemonization (we hope)
+			setTimeout(log.bind(null, "config ok"), 1000);
 		} catch(e) {
 			log("Error reading config: " + e);
-			if (exitOnFailure) process.exit(1);
+			if(exitOnFailure) process.exit(1);
 		}
 	}
 
@@ -104,7 +105,7 @@ function main () {
 		var opts = {
 			stdout: fp,
 			stderr: fp
-		}
+		};
 
 		// Daemonize. At this point, the parent will exit.
 		daemon(opts);
@@ -126,28 +127,28 @@ function main () {
 /**
  * Initialize (or re-initialize) state
  */
-function init_state (CONFIG, STATE) {
-	function clone (o) {
+function init_state(CONFIG, STATE) {
+	function clone(o) {
 		var c = {};
 		for(var x in o) c[x] = o[x];
 		return c;
 	}
 
-	['schedule', 'watch'].forEach(function (type) {
+	['schedule', 'watch'].forEach(function(type) {
 		var state = STATE[type]
 			, config = CONFIG[type]
 			, init = {last_run: new Date("1980/01/01 00:00:00")};
 
 		// Remove all state and kill jobs that are no longer in the config.
-		for (var name in state) {
-			if (!config[name]) {
+		for(var name in state) {
+			if(!config[name]) {
 				// Clear restart timeout
-				if (state[name].timeout) clearTimeout(state[name].timeout)
+				if(state[name].timeout) clearTimeout(state[name].timeout);
 				// Kill running process
-				if (state[name].process) process.kill(state[name].process.pid);
+				if(state[name].process) process.kill(state[name].process.pid);
 				delete state[name];
 			}
-		};
+		}
 
 		// Initialize state & env for remaining jobs
 		for(var name in config) {
@@ -155,59 +156,59 @@ function init_state (CONFIG, STATE) {
 			jobState.env = config[name].env || {};
 			jobState.env.__proto__ = CONFIG.env;
 		}
-	})
+	});
 }
 
 
 /**
  * Main mojo
  */
-function run (CONFIG, STATE) {
+function run(CONFIG, STATE) {
 	log("Custodian v"+VERSION+" starting...");
-	init_state(CONFIG, STATE)
+	init_state(CONFIG, STATE);
 
 	/**
 	 * Check on the jobs in CONFIG.watch, restarting those that aren't running, and
 	 * killing those that are no longer in the config.
 	 */
-	function check_watched_jobs () {
+	function check_watched_jobs() {
 
-		Object.keys(CONFIG.watch).forEach(function (name) {
-			var config   = CONFIG.watch[name]
-				, state = STATE.watch[name];
+		Object.keys(CONFIG.watch).forEach(function(name) {
+			var config = CONFIG.watch[name]
+				, state  = STATE.watch[name];
 				
 			// If job is running
-			if (state.process) {
-				if (config.mem_limit) checkMemoryUsage(name, config.mem_limit, state.process.pid)
-				return
+			if(state.process) {
+				if(config.mem_limit) checkMemoryUsage(name, config.mem_limit, state.process.pid);
+				return;
 			}
 
 			// Job is already scheduled to restart
-			if (state.timeout) return;
+			if(state.timeout) return;
 
-			function restart (first) {
+			function restart(first) {
 				// If we were removed from the config, just exit.
-				if (!CONFIG.watch[name]) return
+				if(!CONFIG.watch[name]) return;
 
 				// if `rate_limit` is set, don't restart the job more than once
 				// every X seconds
-				if (CONFIG.rate_limit) {
+				if(CONFIG.rate_limit) {
 					var now = (new Date).getTime()
-						, rate = CONFIG.rate_limit * 1000
+						, rate = CONFIG.rate_limit * 1000;
 					if(now - state.last_run < rate) {
 						log(name+" was started less than "+CONFIG.rate_limit+" seconds ago, deferring");
-						state.timeout = setTimeout(restart, (rate + state.last_run) - now)
+						state.timeout = setTimeout(restart, (rate + state.last_run) - now);
 						return;
 					}
 				}
 
-				delete state.process
-				delete state.timeout
-				if (!first) log(name + " is not running, restarting");
+				delete state.process;
+				delete state.timeout;
+				if(!first) log(name + " is not running, restarting");
 				spawn(name, config, state, sendNotification).on('exit', restart);
-			};
+			}
 
-			restart(true)
+			restart(true);
 		});
 
 	}
@@ -227,69 +228,71 @@ function run (CONFIG, STATE) {
 		if(cfg.args) cfg.args.forEach(function(it){
 			switch(it) {
 				case 'last_run': cfg.cmd += ' "' + dateFormat(state.last_run, "yyyy-mm-dd HH:MM:ss") + '"'; break;
-				default:         console.log("Unrecognized dyn arg: "+it);
+				default:         console.log("Unrecognized dyn arg: " + it);
 			}
 		});
 
 		spawn(name, cfg, state, sendNotification);
-		state.process.on('exit', function runAfter (code) {
+		state.process.on('exit', function runAfter(code) {
 			delete state.process;
 			// TODO - should sub-jobs run after failure?
-			// if (code) return
+			// if(code) return;
 
 			// find jobs that want to be run after this job, and execute them
-			Object.keys(CONFIG.schedule).forEach(function (next_job_name) {
+			Object.keys(CONFIG.schedule).forEach(function(next_job_name) {
 				var next_job = CONFIG.schedule[next_job_name];
 				var m = /^after (.*)$/.exec(next_job.when);
 				if (!m || m[1] != name) return;
 				run_job(next_job_name);
-			})
-		})
+			});
+		});
 	}
 
 	/**
 	 * Send an email notification of an event
 	 */
 	function sendNotification(kind, name, pid, body) {
-		var from = CONFIG.from_email || CONFIG.admin
-			, to = CONFIG.notify_email || CONFIG.admin;
+		var from = CONFIG.from_email || CONFIG.admin_email || CONFIG.admin
+			, to = CONFIG.notify_email || CONFIG.admin_email || CONFIG.admin;
 
 		mailer.sendMail({
-			to:       to,
-			from:     from,
-			subject:  'Custodian | Process ' + kind + ' (' + name + ')',
-			text:     "Hostname: " + HOSTNAME +
-								"\nProcess: " + name +
-								"\nPID: "+ pid +
-								(body ? "\n\n" + body : ""),
+			to:      to,
+			from:    from,
+			subject: 'Custodian | Process ' + kind + ' (' + name + ')',
+			text:    "Hostname: " + HOSTNAME +
+			         "\nProcess: " + name +
+			         "\nPID: "+ pid +
+			         (body ? "\n\n" + body : ""),
 		},
-		function (err, success) {
-			if (err) log("Failed to send mail to " + to + " " + err)
-			else log("Message sent to " + to)
+		function(err, success) {
+			if(err) {
+				log("Failed to send mail to " + to + " " + err);
+			} else {
+				log("Message sent to " + to);
+			}
 		});
 	}
 
 	/**
 	 * Check watched jobs and run any scheduled jobs every 5 seconds
 	 */
-	function dispatch () {
+	function dispatch() {
 		var now = new Date();
 
-		Object.keys(CONFIG.schedule).forEach(function (x) {
+		Object.keys(CONFIG.schedule).forEach(function(x) {
 			var m = /^every ([\d.]+[smhd])$/.exec(CONFIG.schedule[x].when);
-			if (!m) return
-			var delay = parseDuration(m[1])
-			if (!delay) return
+			if (!m) return;
+			var delay = parseDuration(m[1]);
+			if (!delay) return;
 			if(STATE.schedule[x].last_run <= new Date(now - delay)) {
 				run_job(x);
 			}
-		})
+		});
 
 		check_watched_jobs();
-
 	}
 
-	var interval = CONFIG.check_interval || 5000
+	var interval = CONFIG.check_interval || 5000;
 	STATE.interval = setInterval(dispatch, interval);
 	dispatch();
 }
@@ -298,27 +301,26 @@ function run (CONFIG, STATE) {
  * Spawn a new process using the settings in `cfg` and return it.
  */
 function spawn(name, cfg, state, sendNotification) {
-
-	if (!cfg.cmd) return log('Error: No "cmd" in ' + name + ' cfg: ' + util.format(cfg));
+	if(!cfg.cmd) return log('Error: No "cmd" in ' + name + ' cfg: ' + util.format(cfg));
 
 	// Backwards compatibility with old configs
-	if (Array.isArray(cfg.cmd)) cfg.cmd = cfg.cmd.join(' ');
+	if(Array.isArray(cfg.cmd)) cfg.cmd = cfg.cmd.join(' ');
 
 	// Parse args and expand environment variables
 	var args = shellParse(cfg.cmd)
 		, cmd = args.shift()
-		, cwd = cfg.cwd || process.cwd()
+		, cwd = cfg.cwd || process.cwd();
 
-	args = args.map(function (arg) { return shellExpand(arg, state.env) });
+	args = args.map(function(arg) { return shellExpand(arg, state.env) });
 
 	// Prepare output redirection
 	var stdio = ['ignore']; // No stdin
 	if(cfg.output) {
 		// redirect stdout/stderr into the file specified
 		// file will be opened in append mode
-		if (state.output_fd) fs.closeSync(state.output_fd)
+		if(state.output_fd) fs.closeSync(state.output_fd);
 		state.output = cfg.output;
-		state.output_fd = fs.openSync(shellExpand(cfg.output), 'a')
+		state.output_fd = fs.openSync(shellExpand(cfg.output), 'a');
 
 		stdio[1] = stdio[2] = state.output_fd;
 	} else {
@@ -332,68 +334,68 @@ function spawn(name, cfg, state, sendNotification) {
 		stdio: stdio
 	});
 
-	log("Started " + name + " (pid: " + c.pid + ")")
+	log("Started " + name + " (pid: " + c.pid + ")");
 	state.last_run = (new Date()).getTime();
 
-	var maxtime
-	if (cfg.maxtime && (maxtime = parseDuration(cfg.maxtime))) {
-		state.killtimer = setTimeout(function () {
-			process.kill(c.pid)
-			log('Killed ' + name + " (pid: " + c.pid + ') for exceeding maxtime of ' + cfg.maxtime)
-		}, maxtime)
+	var maxtime;
+	if(cfg.maxtime && (maxtime = parseDuration(cfg.maxtime))) {
+		state.killtimer = setTimeout(function() {
+			process.kill(c.pid);
+			log('Killed ' + name + " (pid: " + c.pid + ') for exceeding maxtime of ' + cfg.maxtime);
+		}, maxtime);
 	}
 
-	c.on('error', sendNotification.bind(null, "error", name, c.pid))
-	c.on('exit', function onExit (code) {
-		log(name + ' finished with code ' + code)
-		if (state.output_fd) {
+	c.on('error', sendNotification.bind(null, "error", name, c.pid));
+	c.on('exit', function onExit(code) {
+		log(name + ' finished with code ' + code);
+		if(state.output_fd) {
 			fs.closeSync(state.output_fd);
 			delete state.output_fd;
 		}
-		if (state.killtimer) {
+		if(state.killtimer) {
 			clearTimeout(state.killtimer);
-			delete state.killtimer
+			delete state.killtimer;
 		}
-		if (!code) {
+		if(!code) {
 			// successful exit
-			return
+			return;
 		}
 		var body = state.output ? "Output is readable in " + state.output : "";
 		sendNotification("returned code " + code, name, c.pid, body);
-	})
+	});
 	return c;
 }
 
-function shellExpand (string, env) {
-	if (string[0] === "'") return string;
-	if (!env) env = process.env;
-	return string.replace(/\$([A-Za-z0-9_]+)/g, function (m) {
-		return env[m.substring(1)] || ''
-	})
+function shellExpand(string, env) {
+	if(string[0] === "'") return string;
+	if(!env) env = process.env;
+	return string.replace(/\$([A-Za-z0-9_]+)/g, function(m) {
+		return env[m.substring(1)] || '';
+	});
 }
 
-function checkMemoryUsage (name, limit, pid) {
+function checkMemoryUsage(name, limit, pid) {
 	var kb_limit = parseSize(limit);
-	if (!kb_limit) return;
-	cproc.exec('ps -o rss ' + pid, function (err, stdout, stderr) {
-		if (err) return log("Failed to get memory usage for " + name + " " + err)
+	if(!kb_limit) return;
+	cproc.exec('ps -o rss ' + pid, function(err, stdout, stderr) {
+		if(err) return log("Failed to get memory usage for " + name + " " + err);
 
-		var rss = Number(stdout.split('\n').filter(Boolean).pop().trim())
+		var rss = Number(stdout.split('\n').filter(Boolean).pop().trim());
 
-		if (isNaN(rss)) return log("Failed to parse memory usage for " + name + " " + stdout)
+		if(isNaN(rss)) return log("Failed to parse memory usage for " + name + " " + stdout);
 
-		if (rss > kb_limit) {
-			process.kill(pid)
+		if(rss > kb_limit) {
+			process.kill(pid);
 			log("Killed " + name + " for exceeding memory threshold: "
-					+ rss + " > " + limit);
+			    + rss + " > " + limit);
 		}
-	})
+	});
 }
 
 /**
  * Parse a size given in kilo/mega/gigabytes into a number of kilobytes
  */
-function parseSize (size) {
+function parseSize(size) {
 	var m = size.toLowerCase().match(/^(\d+)\s*(k|m|g)b?$/);
 	if (!m) return log('Invalid size: ' + size);
 	var n = Number(m[1])
@@ -405,17 +407,17 @@ function parseSize (size) {
 	}
 }
 
-function parseDuration (dur) {
+function parseDuration(dur) {
 	var m = /^([\d.]+)([smhd])$/.exec(dur);
-	if (!m) throw new Error("Invalid duration " + dur)
+	if (!m) throw new Error("Invalid duration " + dur);
 
-	var n = Number(m[1])
-	if (isNaN(n)) throw new Error("Invalid duration " + dur)
+	var n = Number(m[1]);
+	if (isNaN(n)) throw new Error("Invalid duration " + dur);
 
 	switch(m[2]) {
-		case 's': return 1000 * n
-		case 'm': return 60000 * n
-		case 'h': return 3600000 * n
-		case 'd': return 86400000 * n
+		case 's': return 1000 * n;
+		case 'm': return 60000 * n;
+		case 'h': return 3600000 * n;
+		case 'd': return 86400000 * n;
 	}
 }
